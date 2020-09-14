30fea132159f233ebd161d955b31055c4c61a32e
Ben Lewin <benlewin7@gmail.com>
2020-09-14 01:59:31 +0100

    new loading screen

--- linux/main.py
+++ linux/main.py
@@ -1,7 +1,9 @@
-import PyQt5.QtGui
+from PyQt5.QtGui import QMovie
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QFileDialog,
                              QInputDialog, QMessageBox, QLineEdit,
-                             QCheckBox)
+                             QCheckBox, QWidget, QProgressBar, QLabel,
+                             QDialog)
 from subprocess import Popen, PIPE
 from pathlib import Path
 import subprocess
@@ -15,6 +17,35 @@ if os.name == 'posix':
 elif os.name == 'nt':
     # windows os
     program = 'qpdf.exe'
+
+
+class LoadingScreen(QWidget):
+    def __init__(self):
+        super().__init__()
+        # setWindowModality stops user from interacting with parent window
+        # whilst the child window is open
+        self.setWindowModality(Qt.ApplicationModal)
+        self.setFixedSize(64, 64)
+        self.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.CustomizeWindowHint)
+        self.label_animation = QLabel(self)
+
+        self.movie = QMovie('loading.gif')
+
+        self.label_animation.setMovie(self.movie)
+
+        # timer = QTimer(self)
+
+        self.startAnimation()
+        # timer.singleShot(3000, self.stopAnimation)
+
+        self.show()
+
+    def startAnimation(self):
+        self.movie.start()
+
+    def stopAnimation(self):
+        self.movie.stop()
+        self.close()
 
 
 class ExampleApp(QMainWindow, design.Ui_MainWindow):
@@ -31,16 +62,27 @@ class ExampleApp(QMainWindow, design.Ui_MainWindow):
         # self.AESOption.activated[int]
 
     def show_dialog(self, icon=None, text=None, window_title=None):
-        msg = QMessageBox()
+        msg = QMessageBox(self)
         msg.setIcon(icon)
         msg.setText(text)
         msg.setWindowTitle(window_title)
         msg.exec_()
 
+    def show_progress_bar(self):
+        w = QMessageBox(self)
+        w.setText("test")
+        self.progress = QProgressBar()
+        self.progress.setGeometry(200, 80, 250, 20)
+        # w.setFixedSize(100, 100)
+        # w.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.CustomizeWindowHint)
+        w.exec_()
+
+        # self.progress = QProgressBar(self)
+        # self.progress.setGeometry(200, 80, 250, 20)
 
     def open_folder(self):
-    	directory = QFileDialog.getExistingDirectory(self, "Pick a folder")
-    	if directory:
+        directory = QFileDialog.getExistingDirectory(self, "Pick a folder")
+        if directory:
             for file in os.listdir(directory):
                 if file.endswith(".pdf"):
                     filter = os.path.join(directory, file)
@@ -59,6 +101,7 @@ class ExampleApp(QMainWindow, design.Ui_MainWindow):
         self.listWidget.clear()
 
     def show_password(self):
+        # self.show_progress_bar()
         if self.ShowPassword.isChecked():
             self.btnPassword.setEchoMode(QLineEdit.Normal)
         else:
@@ -113,8 +156,10 @@ class ExampleApp(QMainWindow, design.Ui_MainWindow):
             suffix = new_file_path.suffix
             new_filename = new_file_path.stem+identifier+suffix
             try:
+                self.loading_screen = LoadingScreen()
                 subprocess.check_output([program, '--decrypt', '--password={}'.format(passwd),
                                             item, os.path.join(destination, new_filename)])
+                self.loading_screen.stopAnimation()
                 """self.show_dialog(icon=QMessageBox.Information,
                                 text="{} pdf(s) successfully decrypted".format(len(itemsTextList)),
                                 window_title="decrypt")"""
